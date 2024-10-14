aff244b471c2effd6ddd8ed5c530ed9edb336a7a
Product Migrator <genbee@example.com>
2024-10-14 07:58:48 +0000

    Migration result task_id d2d71518-b9b0-41fd-930d-266db24e1658

---
+++ src/main/java/com/codebrickie/gwtjee/server/servlet/GreetingServiceServlet.java
@@ -0,0 +1,28 @@
+package com.codebrickie.gwtjee.server.servlet;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.annotation.WebServlet;
+
+import com.codebrickie.gwtjee.client.GreetingService;
+import com.codebrickie.gwtjee.domain.Greeting;
+import com.codebrickie.gwtjee.server.service.GreetingBean;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = { "/GWTJEE/greet" })
+public class GreetingServiceServlet extends RemoteServiceServlet implements GreetingService {
+
+    @EJB
+    private GreetingBean greetingBean;
+
+    public Greeting greetServer(String input) throws IllegalArgumentException {
+        var serverInfo = escapeHtml(getServletContext().getServerInfo());
+        var userAgent = escapeHtml(getThreadLocalRequest().getHeader("User-Agent"));
+
+        return greetingBean.greet(input, userAgent, serverInfo);
+    }
+
+    private String escapeHtml(String html) {
+        return html == null ? null : html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+    }
+}
